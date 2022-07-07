2e93daed7b3b9dedbec7923794190edfb9d727d2
Phan Ba Huy <huy.pb.2011@gmail.com>
2022-07-07 13:16:44 +0700

    Lai Update lan nua guys

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ $(document).ready(function () {
       {
         breakpoint: 850,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 1,
           slidesToScroll: 1,
           autoplay: false,
           autoPlaySpeed: 1500,
@@ -48,7 +48,7 @@ $(document).ready(function () {
       {
         breakpoint: 850,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 1,
           slidesToScroll: 1,
           autoplay: false,
           autoPlaySpeed: 1500,
@@ -60,3 +60,29 @@ $(document).ready(function () {
     ],
   });
 });
+
+const abc = document.querySelector.bind(document);
+const ABC = document.querySelectorAll.bind(document);
+
+const menuMobile = abc(".background-menu");
+const menuBtn = abc(".menu-icon");
+
+const closeMenu = abc(".close-menu__icon");
+
+const app = {
+  handleEvents: function () {
+    menuBtn.onclick = function () {
+      menuMobile.classList.add("enable");
+      menuMobile.classList.remove("disable");
+    };
+    closeMenu.onclick = function () {
+      menuMobile.classList.remove("enable");
+      menuMobile.classList.add("disable");
+    };
+  },
+  start: function () {
+    this.handleEvents();
+  },
+};
+
+app.start();
